efe9b5a2b43f0216c376a8425f9083cba4534d95
Ksenia Agalakova <agalaksu111@gmail.com>
2024-09-19 22:18:07 +0300

    Modified css for Movie, Pagination, Navbar

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import "./Movie.css";
+import Skeleton from "react-loading-skeleton";
 
 interface MovieProps {
   id: number;
@@ -8,13 +9,16 @@ interface MovieProps {
   poster_path: string;
   release_date: string;
   vote_average: number;
+  loading: boolean;
 }
 
-const Movie: React.FC<MovieProps> = ({ id, title, overview, poster_path, release_date, vote_average }) => {
-  const ringRef = useRef(null);
+const Movie: React.FC<MovieProps> = ({ id, title, overview, poster_path, release_date, vote_average, loading }) => {
+  const ringRef = useRef<HTMLDivElement>(null);
+
+  const [isImageLoading, setIsImageLoading] = useState(true);
 
   const setCSSProgressRing = (rating: number) => {
-    const ring: any = ringRef.current;
+    const ring = ringRef.current;
 
     // Cap the rating between 0 and 10
     rating = Math.max(0, Math.min(10, rating));
@@ -23,7 +27,7 @@ const Movie: React.FC<MovieProps> = ({ id, title, overview, poster_path, release
     const percentage = (rating / 10) * 100;
 
     // Update the CSS variable and the text
-    ring.style.setProperty("--percentage", percentage);
+    ring?.style.setProperty("--percentage", "" + percentage);
   };
 
   useEffect(() => {
@@ -32,22 +36,52 @@ const Movie: React.FC<MovieProps> = ({ id, title, overview, poster_path, release
 
   return (
     <div className="movie-card">
-      <div className="movie-img-container">
-        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-      </div>
+      {(loading || isImageLoading) && <Skeleton height={"300px"} width={"200px"} className="skeleton" />}
+
+      {!loading && (
+        <div className="movie-img-container" style={isImageLoading ? { display: "none" } : {}}>
+          <img
+            src={
+              !poster_path
+                ? "https://parniangostar.com/_next/static/media/imgFallBack.581a9fe3.png"
+                : `https://image.tmdb.org/t/p/w500${poster_path}`
+            }
+            onLoad={() => setIsImageLoading(false)}
+            alt={title}
+          />
+        </div>
+      )}
 
       <div className="movie-info">
-        <h2 className="movie-title">{title}</h2>
-        <div className="movie-rating">
-          <p>Release Date: {release_date}</p>
-          <div className="rating-container">
-            <div ref={ringRef} className="progress-ring" data-id={id} data-rating="7">
-              <span className="rating-text">{vote_average.toFixed(1)}</span>
+        {loading ? (
+          <Skeleton count={1} className="skeleton-title" width={"20vw"} />
+        ) : (
+          <h2 className="movie-title">{title}</h2>
+        )}
+        {loading ? (
+          <>
+            <Skeleton count={3} className="skeleton-info" width={"20vw"} />
+            <Skeleton circle width={30} height={30} className="skeleton-info" />
+          </>
+        ) : (
+          <>
+            <div className="movie-rating">
+              <p className="movie-release-date">Release Date: {release_date || "N/A"}</p>
+              <div className="rating-container">
+                <div ref={ringRef} className="progress-ring" data-id={id} data-rating="7">
+                  <span className="rating-text">
+                    {typeof vote_average === "number" && !isNaN(vote_average)
+                      ? vote_average.toFixed(1) === "0.0"
+                        ? "N/A"
+                        : vote_average.toFixed(1)
+                      : "N/A"}
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-          {/*<p>Rating: {vote_average.toFixed(1)}</p>*/}
-        </div>
-        <p>{overview}</p>
+            <p className="movie-overview">{!overview ? "Description not available at the moment" : overview}</p>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Pagination.css";
 
 interface PaginationProps {
   currentPage: number;
@@ -8,7 +9,46 @@ interface PaginationProps {
 }
 
 const Pagination: React.FC<PaginationProps> = ({ currentPage, moviesPerPage, totalMovies, onPageChange }) => {
-  return <div>Pagination</div>;
+  const totalPages = Math.ceil(totalMovies / moviesPerPage);
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const handlePageClick = (page: number) => {
+    onPageChange(page);
+  };
+
+  const amountOfPages = [];
+  for (let i = 1; i <= totalPages; i++) amountOfPages.push(i);
+
+  return (
+    <div className="pagination">
+      <button className="pagination-button" onClick={handlePrevClick} disabled={currentPage === 1}>
+        <span className="pagination-arrow">&lt;</span>
+      </button>
+      {amountOfPages.map((page) => (
+        <button
+          key={page}
+          className={`pagination-button ${currentPage === page ? "active" : ""}`}
+          onClick={() => handlePageClick(page)}
+        >
+          {page}
+        </button>
+      ))}
+      <button className="pagination-button" onClick={handleNextClick} disabled={currentPage === totalPages}>
+        <span className="pagination-arrow">&gt;</span>
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
