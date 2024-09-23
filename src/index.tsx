eb6c186459a1c9cd501c72bfcccaafcec1c384b8
Ksenia Agalakova <agalaksu111@gmail.com>
2024-09-23 17:47:00 +0300

    style changes

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <SkeletonTheme baseColor="#d9d9d9">
+      <App />
+    </SkeletonTheme>
   </Provider>
 );
