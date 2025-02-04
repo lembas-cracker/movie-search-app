0215c4ca272402b8db685abe08aa79bcf7b1e904
Ksenia Agalakova <agalaksu111@gmail.com>
2025-02-04 22:39:04 +0300

    Fixed misalignment on smaller screens

--- src/components/InfoPopup.tsx
+++ src/components/InfoPopup.tsx
@@ -6,20 +6,21 @@ import { Tooltip as ReactTooltip } from "react-tooltip";
 const InfoPopup: React.FC = () => {
   return (
     <>
-      <span className="nav-text">GitHub source code:</span>
       <ReactTooltip id="my-tooltip" />
       <div
-        className="info-container"
+        className="info-container nav-item"
         data-tooltip-id="my-tooltip"
         data-tooltip-content="Portfolio project with React, TypeScript and Redux Toolkit for fetch requests and state management. Movies are pulled through API requests to themoviedb.org. Other things used are react-loading-skeleton package and react-router library."
       >
-        <span className="info-icon">
+        <span className="info">
           <img
-            src="https://wikis.tid.es/gvp-public/images/thumb/9/9f/Infobox_info_icon_white.svg.png/600px-Infobox_info_icon_white.svg.png"
+            src="https://img.icons8.com/m_outlined/512/FFFFFF/info.png"
             alt=""
             style={{ width: "20px", height: "20px" }}
+            className="info-icon"
           />
         </span>
+        <span className="nav-text">About this project</span>
       </div>
       <ReactTooltip />
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,29 @@
 import InfoPopup from "./InfoPopup";
 import "./Navbar.css";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <div className="nav-container">
-      <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-        <span className="nav-logo">MovieFind</span>
-      </Link>
-      <NavLink
-        to="/movies"
-        style={{ textDecoration: "none" }}
-        className={({ isActive }) => (isActive ? "nav-item active" : "nav-item")}
-      >
-        Movies
-      </NavLink>
-      <Link
-        to="https://github.com/lembas-cracker/movie-search-app"
-        style={{ color: "inherit", textDecoration: "none" }}
-      >
-        <img
-          src="https://img.icons8.com/m_sharp/200/FFFFFF/github.png"
-          alt=""
-          style={{ width: "20px", height: "20px" }}
-        />
-      </Link>
-      <InfoPopup />
-      <span className="nav-text">About this project</span>
+      <div className="nav-wrapper">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="nav-logo">MovieFind</span>
+        </Link>
+        <Link
+          to="https://github.com/lembas-cracker/movie-search-app"
+          style={{ color: "inherit", textDecoration: "none" }}
+          className="nav-item"
+        >
+          <img
+            src="https://img.icons8.com/m_sharp/200/FFFFFF/github.png"
+            className="nav-icon"
+            alt=""
+            style={{ width: "20px", height: "20px" }}
+          />
+          <span className="nav-text">GitHub source code</span>
+        </Link>
+        <InfoPopup />
+      </div>
     </div>
   );
 };
